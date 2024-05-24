c704a8e4e2870b090cb73eab20bfcc2486135709
fastfabien <fastfabien56@gmail.com>
2024-05-24 21:16:36 +0300

    Font: resolve load error

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
